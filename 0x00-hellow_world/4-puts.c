4d21d8340934cf8d66ab583fa6e4ba5d3606d439
abedALXproject <cletalivingston2@gmail.com>
2023-07-22 06:33:28 -0400

    Preprocessor

---
+++ 0x00-hellow_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: This programming is like building a multilinqual puzzle
+ * Return: alway 0 (success)
+*/
+int main(void)
+{
+	puts("\"pograming is like building a multilinqual puzzle);
+	return (0);
+}
--- 0x00-hellow_world/main.c
+++ 0x00-hellow_world/main.c
@@ -2,9 +2,11 @@
 
 /**
  * main - Entry point
- * Return: always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	return (0);
+    return (0);
 }
+
