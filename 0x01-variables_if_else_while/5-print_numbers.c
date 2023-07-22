0a0da8462dc160357900b64cf9e6ed605a58c3c4
abedALXproject <cletalivingston2@gmail.com>
2023-07-22 15:34:49 -0400

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: this print 0 to 10.
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int w;
+
+	for (w = 0; w < 10; w++)
+	{
+		printf("%d", w);
+	}
+	printf("\n");
+	return (0);
+}
