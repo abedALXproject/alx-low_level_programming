e128d618f69d043decb17381d572ff176ed69e3d
abedALXproject <cletalivingston2@gmail.com>
2023-07-22 08:03:10 -0400

    Hello, printf

---
+++ 0x00-hellow_world/5-printf.c
@@ -0,0 +1,16 @@
+/**
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+*/
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This print grammar, but the outcome is a piece of art
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piecve of art,\n");
+	return (0);
+}
