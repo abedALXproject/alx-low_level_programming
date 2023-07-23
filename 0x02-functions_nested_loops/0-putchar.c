b49c75b7cdbb55c91572b3fa7cd06e8fbb9a3564
abedALXproject <cletalivingston2@gmail.com>
2023-07-23 06:24:05 -0400

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: It prints _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
